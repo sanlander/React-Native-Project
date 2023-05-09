5fc56ea2c701fd3c5d36bcc16755b45d590aaf23
Alexandr Gorshkoder <88847067+sanlander@users.noreply.github.com>
2023-05-09 21:59:39 +0300

    create LoginScreen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
 
 export default function App() {
   return (
     <>
       <RegistrationScreen />
+      {/* <LoginScreen /> */}
       <StatusBar style="auto" />
     </>
   );
